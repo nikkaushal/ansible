43f22e6d1f056e77f34fa6c197c8ddc822918b34
nikkaushal <nikkaushal@gmail>
2021-02-13 15:56:33 -0500

    roboshop-project

--- roboshop-project/make-inventory.sh
+++ roboshop-project/make-inventory.sh
@@ -4,6 +4,6 @@
 for component in frontend cart catalogue user rabbitmq shipping payment mysql mongo redis; do
   IP=$(aws ec2 describe-instances --filters Name=tag:Name,Values=${component} Name=instance-state-name,Values=running | jq '.Reservations[].Instances[].PrivateIpAddress'|xargs)
   if [ -n "${IP}" ]; then
-   echo $IP component=${component} >>inventory
+   echo $IP component=${component} ansible_user=centos ansible_password=DevOps321 >>inventory
   fi
 done
